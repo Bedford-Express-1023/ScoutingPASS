017071f89934ba68f2d0b07af764765548d496e6
gavinda18 <98656609+gavinda18@users.noreply.github.com>
2022-01-29 15:48:56 -0500

    Update RR_config.js
    Put Match Level and Robot back in AGAIN!

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -18,9 +18,18 @@ var config_data = `
         "required":"true",
         "disabled":"true"
       },
-      "Match Level":{
-	"code":"l",
-        "type":" "
+       "Match Level":{
+        "code":"l",
+        "type":"level",
+        "choices":{
+          "qm":"Quals<br>",
+          "ef":"Eighth-Final<br>",
+          "qf":"Quarter-Final<br>",
+          "sf":"Semi-Final<br>",
+          "f":"Final"
+        },
+        "defaultValue":"qm",
+        "required":"true"
       },
       "Match #":{
         "code":"m",
@@ -30,8 +39,17 @@ var config_data = `
         "required":"true"
       },
       "Robot": {
-       "code":"r",
-       "type":" "
+        "code":"r",
+        "type":"robot",
+        "choices":{
+          "r1":"Red-1",
+          "b1":"Blue-1<br>",3
+          "r2":"Red-2",
+          "b2":"Blue-2<br>",
+          "r3":"Red-3",
+          "b3":"Blue-3"
+        },
+        "required":"true"
       },
       "Team #": {
         "code":"t",
