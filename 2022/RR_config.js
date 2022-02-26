c8e5e02ecff5ec9c5f478cd2d0d180f1720ff387
BedfordExpress <59457877+BedfordExpress@users.noreply.github.com>
2022-02-26 15:03:13 -0500

    Update RR_config.js

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -5,28 +5,28 @@ var config_data = `
   "elements":{
     "prematch": {
       "Scouter Name": {
-        "code":"scouter",
+        "code":"scout",
         "type":"scouter",
         "size":5,
         "maxSize":30,
         "required":"true"
       },
       "Event":{
-        "code":"event",
+        "code":"e",
         "type":"event",
         "defaultValue":"2022flwp",
         "required":"true",
         "disabled":"true"
       },
       "Match #":{
-        "code":"match",
+        "code":"m",
         "type":"match",
         "min":1,
         "max":100,
         "required":"true"
       },
      "Robot": {
-        "code":"robot",
+        "code":"r",
         "type":"robot",
         "choices":{
           "r1":"Red-1",
@@ -39,14 +39,14 @@ var config_data = `
         "required":"true"
       },
       "Team #": {
-        "code":"team num",
+        "code":"t",
         "type":"team",
         "min":1,
         "max":99999,
 	"required":"true"
       },
       "Preload": {
-	"code":"preload",
+	"code":"p",
 	"type":"bool",
 	"required":"true"
       },
