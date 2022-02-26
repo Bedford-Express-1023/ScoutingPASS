9f82b63456b8a89c64ccee64135ffc354dafce74
BedfordExpress <59457877+BedfordExpress@users.noreply.github.com>
2022-02-26 15:18:58 -0500

    Update RR_config.js

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -12,7 +12,7 @@ var config_data = `
         "required":"true"
       },
       "Event":{
-        "code":"e",
+        "code":"event",
         "type":"event",
         "defaultValue":"2022flwp",
         "required":"true",
