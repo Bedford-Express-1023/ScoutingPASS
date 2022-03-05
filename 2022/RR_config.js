8bffc6fc51167e103b11c75cbced5393e938fe39
gavinda18 <98656609+gavinda18@users.noreply.github.com>
2022-03-05 07:59:05 -0500

    Update RR_config.js

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -28,7 +28,6 @@ var config_data = `
      "Robot": {
         "code":"r",
         "type":"robot",
-	"required":"true",
         "choices":{
           "r1":"Red-1",
           "b1":"Blue-1<br>",
@@ -37,6 +36,7 @@ var config_data = `
           "r3":"Red-3",
           "b3":"Blue-3"
         },
+	"required":"true"
       },
       "Team #": {
         "code":"t",
