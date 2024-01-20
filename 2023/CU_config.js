71f510c611b8cfd1a45a503737776cb79ed81488
gavinda18 <98656609+gavinda18@users.noreply.github.com>
2024-01-20 10:50:36 -0500

    Update CU_config.js
    changed NA

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -73,7 +73,7 @@ var config_data = `
         "Front": "Front 3<br>",
         "Middle Line": "Middle Line<br>",
         "Both": "Both<br>",
-        "N/A": "Not attempted"
+        "Not Attempted": "Not attempted"
       },
       "defaultValue": "x"
     }
@@ -102,7 +102,7 @@ var config_data = `
         "Source": "Source<br>",
         "Ground": "Ground<br>",
         "Both": "Both<br>",
-        "NA": "Not Attempted"
+        "Not Attempted": "Not Attempted"
       },
       "defaultValue": "x"
     }
@@ -115,7 +115,7 @@ var config_data = `
         "Parked": "Parked<br>",
         "Onstage": "Onstage<br>",
         "abf": "Attempted but Failed<br>",
-        "NA": "Not Attempted"
+        "Not Attempted": "Not Attempted"
       },
       "defaultValue": "x"
     },
